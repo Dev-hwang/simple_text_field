cc086040734e0e318f1c1a0280daa8157820e0e4
pravera <hwj930513@naver.com>
2021-03-31 21:43:34 +0900

    Apply Material design textTheme

--- lib/simple_text_field_label.dart
+++ lib/simple_text_field_label.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 /// A widget that implements the label of [SimpleTextField].
 class SimpleTextFieldLabel extends StatelessWidget {
   final String text;
-  final TextStyle style;
-  final TextStyle asterStyle;
+  final TextStyle? style;
+  final TextStyle? asterStyle;
   final EdgeInsetsGeometry padding;
   final bool important;
 
   const SimpleTextFieldLabel(this.text, {
     Key? key,
-    this.style = const TextStyle(fontSize: 18.0, color: const Color(0xFF333333)),
-    this.asterStyle = const TextStyle(fontSize: 18.0, color: const Color(0xFFA565E4)),
+    this.style,
+    this.asterStyle,
     this.padding = const EdgeInsets.only(top: 16.0, bottom: 8.0),
     this.important = false
   })  : super(key: key);
@@ -23,9 +23,11 @@ class SimpleTextFieldLabel extends StatelessWidget {
       child: RichText(
         text: TextSpan(
           children: <TextSpan>[
-            TextSpan(text: text, style: style),
+            TextSpan(text: text, style: style
+                ?? Theme.of(context).textTheme.subtitle1),
             (important)
-                ? TextSpan(text: '\t*', style: asterStyle)
+                ? TextSpan(text: '\t*', style: asterStyle
+                ?? Theme.of(context).textTheme.subtitle1?.copyWith(color: Theme.of(context).primaryColor))
                 : TextSpan(text: '')
           ],
         ),
